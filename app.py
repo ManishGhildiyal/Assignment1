4dd03f4601126af8929f32578ea8a4191439273a
Manish Ghildiyal <146653208+ManishGhildiyal@users.noreply.github.com>
2025-05-23 21:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,7 @@ def get_tickets():
     return jsonify({'message': 'Thank you! You will be redirected.', 'url': event_url})
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
